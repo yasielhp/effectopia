65e1f3d8c269374bfe020c328d1650531f22fe72
Yasiel Hernández Portal <yasielhp@gmail.com>
2022-07-22 15:58:47 +0100

    code script 2

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,4 @@ import { ChainId } from "@thirdweb-dev/react";
 
 export const activeChainId = ChainId.Rinkeby;
 export const activeChainName = Object.keys(ChainId).find(key => ChainId[key] === activeChainId);
+export const myAddress = "0x779f1ac2aCfbA9739A06032997eF8742f73622E5";
