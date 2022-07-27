963bdf21dc7187a5f4bf27c7e71bfa92cab873c7
Yasielhp <yasielhp@gmail.com>
2022-07-27 15:56:12 +0100

    homepage ok mobile

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,22 +2,22 @@ import { Link } from 'react-router-dom';
 export const HomePage = () => {
   return (
     <>
-      <section className="flex flex-row ">
-        <div className="flex flex-col justify-center w-full lg:w-1/2">
-          <h1 className="w-4/5 text-5xl font-semibold text-left mb-7">
+      <section className="flex flex-col-reverse lg:flex-row w-full ">
+        <div className="flex flex-col justify-center lg:w-1/2">
+          <h1 className="lg:w-4/5 text-3xl lg:text-5xl font-semibold text-left mb-7">
             Community as unique as its users
           </h1>
-          <h2 className="text-4xl font-light mb-7">
+          <h2 className="text-2xl lg:text-4xl font-light mb-7">
             Learn. Enjoy. Explore the future of online communities
           </h2>
-          <p className="w-11/12 text-lg font-normal mb-7">
+          <p className="lg:w-11/12 text-base lg:text-lg font-normal mb-7">
             Effectopia is a revolutionary virtual community; a
             real-time peer-to-peer collaboration platform for teachers
             and students powered by the #MassiveAdoption movement.
             Join the community and join us in our mission to provide
             diversity, individuality and freedom like never before.
           </p>
-          <div className="flex flex-col lg:flex-row  w-11/12 gap-x-5 gap-y-5">
+          <div className="flex flex-col lg:flex-row lg:w-11/12 gap-x-5 gap-y-5">
             <Link
               className="w-full h-11 text-orange-100 bg-orange-500 hover:bg-orange-600 focus:ring-1 focus:outline-none focus:ring-orange-400 font-medium rounded-lg text-base px-5 py-2.5 text-center mr-3 md:mr-0"
               to="/membership"
@@ -32,9 +32,9 @@ export const HomePage = () => {
             </Link>
           </div>
         </div>
-        <div className="w-1/2 invisible lg:visible">
+        <div className="flex flex-col justify-center w-full h-1 lg:w-1/2 invisible lg:h-full lg:visible">
           <img
-            className="w-full h-full pointer-events-none select-none invisible lg:visible"
+            className="w-full h-full pointer-events-none select-none"
             src="img/hero.svg"
             alt="hero image"
           />
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -14,7 +14,7 @@ export const AppRouter = () => {
   return (
     <>
       <Navbar />
-      <main className="flex flex-col items-center justify-center w-screen h-full px-6 sm:px-10 md:px-20 lg:px-px34">
+      <main className="flex flex-col items-center justify-center w-screen h-full px-6 lg:px-34">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="membership" element={<MembershipPage />} />
